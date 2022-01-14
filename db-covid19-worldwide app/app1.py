93c97ba3eeeae49f38d57878b80f7527082505f1
Miguel Garcia <asiesmiguel@gmail.com>
2022-01-14 16:14:14 +0100

    Update app1.py
    testing a modification

--- db-covid19-worldwide app/app1.py
+++ db-covid19-worldwide app/app1.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server# only for heroku
+#
 
 red='#cc1f04'
 yellow='#e0d907'
